5a2cc78893d14eeb9ba602b9153771798418afcb
swayam3042w <69586150+swayam3042w@users.noreply.github.com>
2020-12-14 18:48:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine=Matter.Engine ;
+const World=Matter.World ;
+const Bodies=Matter.Bodies ;
+
+var engine,world;
+var object;
+var ground;
+var ball;
+
+function setup() {
+  var canvas = createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world;
+  
+var options={
+  isStatic:true
+}  
+
+  //object=Bodies.rectangle(200,100,50,50,options);
+  ground=Bodies.rectangle(200,390,200,20,options)
+  //World.add(world,object);
+  World.add(world,ground)
+
+  var ball_options ={
+    restitution: 1.0
+  }
+  
+  ball=Bodies.circle(200,100,20,ball_options)
+  World.add(world,ball)
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+  rectMode(CENTER); 
+  //rect (object.position.x,object.position.y,50,50);
+  rect (ground.position.x,ground.position.y,400,20);
+  ellipseMode (RADIUS)
+  ellipse (ball.position.x,ball.position.y,20,20);
+  drawSprites();
+}
